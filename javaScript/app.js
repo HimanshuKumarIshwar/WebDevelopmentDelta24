d6d1af2c79eb0cda6f78d8cd019c95f36a552556
Himanshu Kumar <himanshuishwar399@gmail.com>
2024-03-12 23:38:49 +0530

    Basic of JS

---
+++ javaScript/app.js
@@ -0,0 +1,408 @@
+// console.log("hello world");
+
+// let a = 10;
+// let b = 5;
+// console.log("sum is : ", a+b);
+
+// let pencilPrice = 10;
+// let erasorPrice = 5;
+
+// console.log(`the total price is : ${pencilPrice + erasorPrice} Rupesse`);
+
+
+
+//Arithmatic Operator
+// let a = 10;
+// let b = 5;
+// // console.log(a+ b);
+// console.log( a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// comparison Operators
+
+// let age = 18;
+
+// console.log(age >= 18);
+
+
+// console.log(5 == 5);
+// console.log(5 == 6);
+// console.log(5 != 6);
+
+// console.log( 5 == '5');
+
+// let n = 5;
+// let str = '5';
+
+// console.log(n === str);
+
+// console.log('a' > '1');
+
+
+
+
+
+
+//=============================================================
+//logical operator
+//=============================================================
+
+// let age = 19;
+
+// if(age > 18){
+//     console.log("You can vote");
+//     console.log("you can drive");
+// }else {
+//     console.log("You can't vote");
+// }
+
+//   console.log("Hello world");
+
+
+// let firstName = "Himanshu";
+// if(firstName === "Himanshu"){
+//     console.log(`welcome ${firstName}`);
+// }
+
+
+// +++++++++++++++++++++++++++++++++++++++++
+// Traffic Ligth System
+// ++++++++++++++++++++++++++++++++++++++++
+// let color = "green";
+
+// if(color === "red"){
+//   console.log("Stop light color is red");
+// }
+// else if(color === 'yellow'){
+//     console.log("slow down light color yellow");
+// }
+// else if(color === 'green'){
+//     console.log("Go light color is green");
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++
+// Marks Sytem
+// ++++++++++++++++++++++++++++++++++++++++
+
+
+// let marks = 8;
+
+// if(marks >= 80){
+//     console.log("Your grade A+");
+// } else if(marks >= 60 && marks < 80){
+//  console.log("your grade A");
+// }else if(marks >= 33 &&  marks < 60){
+//    console.log("Your grade is B");
+// }else if(marks < 33){
+//     console.log("Best of Luck tRy another next Chance");
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++
+// Month weather conition
+// ++++++++++++++++++++++++++++++++++++++
+
+// let month = "january";
+
+// if(month == "january"){
+//     console.log("winter is here");
+// }
+// else if(month == "April"){
+//     console.log("Summer is here");
+// }
+
+
+
+// let age = 17;
+// if(age >= 18){
+//     console.log("You can Vote");
+// } else {
+//     console.log("You cannot vote");
+// }
+
+
+// let color = "blue";
+
+
+// if(color === "red"){
+//   console.log("Stop light color is red");
+// }
+// else if(color === 'yellow'){
+//     console.log("slow down light color yellow");
+// }
+// else if(color === 'green'){
+//     console.log("Go light color is green");
+// }
+// else {
+//     console.log("trafic light is broken");
+// }
+
+//   let size = "XL";
+
+//   if(size === "XL"){
+//     console.log("Price of popcorn is: Rs 250");
+//   } else if(size === "L"){
+//     console.log("Price of popcorn is: Rs 200");
+//   } else if(size === "M"){
+//     console.log("Price of popcorn is: Rs 100");
+//   } else {
+//     console.log("Price of popcorn is: Rs 50");
+//   }
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+// Nested if else
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//  let marks = 45;
+
+//  if(marks >= 33){
+//     console.log("Pass");
+//     if(marks >= 80){
+//         console.log("Grade O");
+//     }else {
+//         console.log("Grad  A");
+//     }
+//  }else {
+//     console.log("Better luck Next Time");
+//  }
+ 
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+// Logical Operator
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// let marks = 90;
+
+// if(marks >= 30 && marks >= 80){
+//     console.log("Pass");
+//     console.log("A");
+// }
+
+
+//   let str = "apple";
+
+//  if(str.startsWith("a") && str.length > 3){
+//     console.log("Good String");
+//  }else {
+//     console.log("Bad String");
+//  }
+
+
+
+// let num = 12;
+// if((num % 3 === 0) && ((num+1 == 15) || (num-1 == 11))){
+//     console.log("safe");
+// }else {
+//     console.log("Unsafe");
+// }
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+// truthy and falsy value
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// if(""){
+//     console.log("it has true value");
+// }else{
+//     console.log("it has false value");
+// }
+
+//let str = "";
+
+// if(str){
+//     console.log("string is not empty");
+// }
+// else {
+//     console.log("string is  empty");
+// }
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+// switch statement
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// let color = "red";
+
+// switch(color){
+//     case "red":
+//          console.log("Stop");
+//          break;
+//     case "yellow":
+//         console.log("slow Down");
+//         break;
+//     case "green":
+//         console.log("Go");
+//         break;
+//     default: 
+//     console.log("Broken Light");
+
+// }
+
+
+// let dayOfWeek = 5;
+
+// switch(dayOfWeek){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//         case 2:
+//         console.log("Tuesday");
+//         break;
+//         case 3:
+//         console.log("Wednesday");
+//         break;
+//         case 4:
+//         console.log("Thursday");
+//         break;
+//         case 5:
+//         console.log("Friday");
+//         break;
+//         case 6:
+//         console.log("saturday");
+//         break;
+//         case 7:
+//         console.log("Sunday");
+//         break;
+//         default:
+//         console.log("Invalid day");
+// }
+
+
+//alert("Hello user");
+//  let userName = prompt("enter you name");
+//  alert(`welcome ${userName}`);
+
+// let firstName = prompt("Enter first Name");
+// let lastName = prompt('Enter last Name');
+
+// alert(`user name is ${firstName + lastName}`)
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+// Assigment Question
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// let num = 100;
+
+// if( num % 10 === 0){
+//     console.log("good");
+// }else {
+//     console.log("bad");
+// }
+
+
+
+
+// let userName = prompt("Enter your Name:");
+// let age = prompt("Enter you age:")
+
+// alert(`${userName} is ${age} years old`);
+
+
+
+
+
+// let quarterOfYears = 3;
+
+// switch(quarterOfYears){
+//     case 1:
+//         console.log(`January, February, March`);
+//         break;
+//     case 2:
+//         console.log(`April, May, June`);
+//         break;
+//         case 3:
+//         console.log(`July, August, September`);
+//         break;
+//         case 4:
+//         console.log(`October, November, December`);
+//         break;
+//         default:
+//             console.log("Invalid querter");
+// }
+
+
+
+
+// let str = "appleBest";
+
+// if((str[0] == 'A' || str[0] == 'a') && str.length > 5){
+//     console.log("Golden String");
+// }else {
+//     console.log("Not a Golden String");
+// }
+
+// let num1 = 10000;
+// let num2 = 120;
+// let num3 = 44;
+
+// if(num1 > num2){
+//     if(num1 > num3){
+//         console.log(`num1 is Greater Number: ${num1}`);
+//     }else {
+//         console.log(`num3 is Greater Number: ${num3}`);
+//     }
+// }else {
+//     if(num2 > num3){
+//         console.log(`num2 is Greater Number: ${num2}`);
+//     }else {
+//         console.log(`num3 is Greater Number: ${num3}`);
+//     }
+// }
+
+
+let num1 = 329;
+let num2 = 478529;
+
+if((num1 % 10) === (num2 % 10) ){
+    console.log("same last Digit");
+}else {
+    console.log(" last Digit is Not same");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
