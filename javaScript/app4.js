e53b612eabfdf7a093de7d17fe5b01e451a12a61
Himanshu Kumar <himanshuishwar399@gmail.com>
2024-03-20 22:25:01 +0530

    Object of JavaScript

---
+++ javaScript/app4.js
@@ -0,0 +1,201 @@
+// const student  =  {
+//     name: "Himanshu",
+//     age: 23,
+//     marks: 94.4,
+//     city: "patna"
+// }
+
+
+//  const item = {
+//     price: 100.99,
+//     discount: 50,
+//     colors:  ["red", "pink", "white"]
+//  }
+
+//  const post = {
+//     username: "@himanshuKumar",
+//     content: " this is Education Post",
+//     likes: 44,
+//     reposts: 3,
+//     tages: ["@apnaCollege", "@delta"]
+//  };
+
+// console.log(post.username);
+// console.log(post["content"]);
+// console.log(post["likes"]);
+// console.log(post["reposts"]);
+
+
+
+const obj = {
+   1: "a",
+   2: "b",
+   3: "c",
+   true: "d",
+   null: "f",
+   undefined: "g"
+
+}
+
+// console.log(obj[1]);
+// console.log(obj["2"]);
+ //console.log(obj.undefined);
+ //console.log(obj[true]);
+
+// const student = {
+//    name: "Himanshu",
+//    age: 23,
+//    marks: 94.4,
+//    city: "Delhi"
+// }
+
+// console.log(student.city);
+// student.city = "Mumbai"
+// console.log(student.city);
+// student.gender = "Male";
+// student.marks = "A";
+// console.log(student.gender);
+ //delete student.marks;
+
+
+//  const classInfo  = {
+//    Himanshu: {
+//       grade: "A+",
+//       city: "Delhi"
+//    },
+//    Hima: {
+//       grade: "A",
+//       city: "pune"
+
+//    },
+//    Ram: {
+// grade: "O",
+// city: "Banaras"
+//    }
+//  }
+
+
+//  console.log(classInfo.Himanshu.city);
+
+// const classInfo = [ 
+//    {
+//       name: "Himanshu",
+//       grade: "A",
+//       city: "delhi"
+
+// },
+// {
+//    name: "radaha",
+//    grade: "A++",
+//    city: "Dwarka"
+// },
+// {
+//    name: "Balram",
+//    grade: "AO",
+//    city: "Bharatpur"
+// }
+// ]
+
+//  console.log(classInfo[0].grade);    
+
+// console.log(Math.PI);
+// console.log(Math.E);
+//console.log(Math.random());
+// console.log(Math.abs(-5.23));
+// console.log(Math.pow(2,3));
+// console.log(Math.floor(9.999));
+// console.log(Math.ceil(9.1));
+
+
+// let step = Math.random();
+// step *=10;
+// console.log(Math.floor(step)+ 1);
+
+
+// let num =Math.floor(Math.random()* 100)  + 1;
+// console.log(num);
+
+// let num2 =Math.floor(Math.random()* 5) + 1
+// console.log(num2);
+
+//  let num3 = Math.floor(Math.random() * (25-21 + 1) ) + 21;
+
+// console.log(num3);
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Guessing Game
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// const max = prompt("Enter the max Number");
+
+// const random = Math.floor(Math.random() * max) +1;
+// let guess = prompt("Guess The Number");
+// while(true){
+//    if(guess == "quit"){
+//       console.log("Number is quit");
+//       break;
+//    } else if(guess == random){
+//       console.log("Congrates You win the game. Guess Number was", random);
+//       break;
+//    }else if(guess < random){
+//       guess = ("hint: You number is too small! Try again");
+//    }else if(guess > random){
+//       guess = ("hint: You number is too large! Try again");
+//    }
+// }
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++
+// Assigment Question
+//++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// const random = Math.floor(Math.random() * 6) +1;
+// console.log(random);
+
+
+
+// const car  = {
+//     name: "XUV 700",
+//     model: 2024,
+//     color: "Black"
+// }
+
+// console.log(car.name);
+// console.log(car["name"]);
+// let carName = "name";
+// console.log(car[carName]);
+
+
+
+// const person = {
+//    name: "Himanshu Kumar",
+//    age: 23,
+//    city: "Patna"
+// }
+
+// console.log(person.city);
+// person.city = "New York";
+// person.country = "'United States"
+// console.log(person.country);
+
+
+
+
+
+
+
+
+
+
+              
+
+
+
+
+
+
+
