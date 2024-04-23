15060760f25920d6baeb35e5c33e8e0dbb885c99
Himanshu Kumar <himanshuishwar399@gmail.com>
2024-04-24 00:22:09 +0530

    RestFull Api

---
+++ Backend/RESTfullApi/index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  {
+    id: uuidv4(),
+    username: "apnacollege",
+    content: "I love codeing",
+  },
+  {
+    id: uuidv4(),
+    username: "Himanshu",
+    content: "I love codeing and my name Hiamsnhu",
+  },
+  {
+    id: uuidv4(),
+    username: "Hima",
+    content: "I got selected my first Intership",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Server working well");
+});
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ username, content, id });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
+});
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
+});
+app.listen(PORT, () => {
+  console.log(`App is listening at port: ${PORT}`);
+});
